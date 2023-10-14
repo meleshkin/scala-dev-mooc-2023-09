8a4c2df3665fc4fc0fd7cc311aa368126c03d8b1
meleshkin <mikhail.meleshkin@gmail.com>
2023-10-14 19:20:04 +0300

    Option impl.

--- src/main/scala/module1/03-introducing_fp_concepts.scala
+++ src/main/scala/module1/03-introducing_fp_concepts.scala
@@ -221,6 +221,23 @@ object hof{
        case Some(v) => f(v)
        case None => None
      }
+
+    /**
+     *
+     * Реализовать метод printIfAny, который будет печатать значение, если оно есть
+     */
+    def printIfAny(): Unit = if (!isEmpty) println(get)
+
+    /**
+     *
+     * Реализовать метод filter, который будет возвращать не пустой Option
+     * в случае если исходный не пуст и предикат от значения = true
+     */
+    def filter(predicate: T => Boolean): Option[T] = this match {
+      case Some(v) => if (predicate(v)) Some(v) else None
+      case None => None
+    }
+
   }
 
   val opt: Option[Int] = ???
@@ -233,33 +250,16 @@ object hof{
 
   object Option{
     def apply[T](v: T): Option[T] = Some(v)
+
+    /**
+     *
+     * Реализовать метод zip, который будет создавать Option от пары значений из 2-х Option
+     */
+    def zip[T, R](first: Option[T], second: Option[R]): Option[(T, R)] = {
+      if (first.isEmpty || second.isEmpty) None else Some(first.get, second.get)
+    }
   }
 
-
-
-
-
-
-
-
-
-  /**
-   *
-   * Реализовать метод printIfAny, который будет печатать значение, если оно есть
-   */
-
-
-  /**
-   *
-   * Реализовать метод zip, который будет создавать Option от пары значений из 2-х Option
-   */
-
-
-  /**
-   *
-   * Реализовать метод filter, который будет возвращать не пустой Option
-   * в случае если исходный не пуст и предикат от значения = true
-   */
 
  }
 
