6dbdaa9e8e33421302d6b2529ce9e034a41aecc9
meleshkin <mikhail.meleshkin@gmail.com>
2024-02-24 16:15:49 +0300

    Impl. webinar 12 homework

--- src/main/scala/scala3_2/homework.scala
+++ src/main/scala/scala3_2/homework.scala
@@ -1,59 +1,67 @@
 package scala3_2
 
+import scala3_2.homework2.{CompletionArg, complete}
 
-/*
-object homework1 {
-  extension (x: String)
-    def + ?????
 
-    @main def part1Ex(): Unit ={
-      println("1" + "33")
-    }
+object homework1 extends App {
+  extension (x: String) {
+    def ++(y: String): Int = (x + y).toInt
+  }
+    println("1" ++ "33")
+
+
 }
 
 object homework2 {
   enum CompletionArg {
-    ???
-    //см приведенную ссылку
+    case FromStringValue(s: String)
+    case FromIntValue(i: Int)
+    case FromFloatValue(f: Float)
   }
 
   object CompletionArg {
-    given fromString: Conversion[String, CompletionArg] = ???
+    given fromString: Conversion[String, CompletionArg] = FromStringValue(_)
 
-    given fromInt: Conversion[Int, CompletionArg] = ???
+    given fromInt: Conversion[Int, CompletionArg] = FromIntValue(_)
 
-    given fromFloat: Conversion[Float, CompletionArg] = ???
+    given fromFloat: Conversion[Float, CompletionArg] = FromFloatValue(_)
   }
-  import CompletionArg.*
 
-  @main def part2Ex(): Unit ={
-    println(Completions.complete("String"))
-    println(Completions.complete(1))
-    println(Completions.complete(7f))
-  }
+  def complete(x: CompletionArg) = x match
+    case CompletionArg.FromStringValue(s) => s
+    case CompletionArg.FromIntValue(i) => i.toString
+    case CompletionArg.FromFloatValue(f) => f.toString
 }
+
+      @main def part2Ex(): Unit = {
+        println(complete("String"))
+        println(complete(1))
+        println(complete(7f))
+      }
 
 
 object homework3 {
   opaque type Logarithm = Double
 
-  object Logarithm{
-    //см приведенную ссылку
+  object Logarithm {
+    def apply(d: Double): Logarithm = math.log(d)
   }
 
   extension (x: Logarithm)
-    def toDouble: ???
-    def + (y: Logarithm): ???
-    def * (y: Logarithm): ???
+    def toDouble(): Logarithm =  math.exp(x)
+    def + (y: Logarithm): Logarithm = Logarithm(math.exp(x) + math.exp(y))
+    def * (y: Logarithm): Logarithm = x * y
 
 
   @main def part3Ex(): Unit ={
-    import Logarithm
+    import Logarithm._
 
     val l = Logarithm(1.0)
     val l2 = Logarithm(2.0)
     val l3 = l * l2
-    val l4 = l + l2
+    val l4 = l + l3
+
+    println(l4)
 
   }
-}*/
+}
